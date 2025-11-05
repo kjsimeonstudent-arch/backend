0790bcf0aea2b0c3b8daf246088b582d64728c8f
Kenzhin <kjsimeon.student@ua.edu.ph>
2025-11-05 13:45:59 +0800

    Revert version 1

--- registration/admin.py
+++ registration/admin.py
@@ -3,23 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import Registration
 
-@admin.register(Registration)
-class RegistrationAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'email', 'organization', 'created_at']
-    list_filter = ['created_at', 'organization']
-    search_fields = ['first_name', 'last_name', 'email', 'organization']
-    readonly_fields = ['created_at', 'updated_at']
-    list_per_page = 25
-    
-    fieldsets = (
-        ('Personal Information', {
-            'fields': ('first_name', 'last_name', 'email', 'phone')
-        }),
-        ('Organization', {
-            'fields': ('organization',)
-        }),
-        ('Timestamps', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',)
-        }),
-    )
+admin.site.register(Registration)
