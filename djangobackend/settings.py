b450ff0da2940ef335e463a2f2073bd636c7b120
Kenzhin <kjsimeon.student@ua.edu.ph>
2025-11-05 10:12:59 +0800

    Add CSRF trusted origins for external domain at bottom part

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#!
+CSRF_TRUSTED_ORIGINS = [
+    "https://peitelbackend.onrender.com",
+]
